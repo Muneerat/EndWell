3938be9e81873985059e131a2c1d3532a22c3016
Muneerat <oladipomunirat@gmail.com>
2024-11-02 04:52:07 +0100

    Add: add shadcn

--- src/app/dashboard/components/mainmenu.jsx
+++ src/app/dashboard/components/mainmenu.jsx
@@ -3,19 +3,19 @@ import React from "react";
 
 export default function MainMenu() {
   return (
-    <div className="flex justify-between w-full">
-      <div className="w-full flex ">
-        <h1>Hello Osa,</h1>
-        <TextInput
-          className="w-full block"
-          label="text"
-          id="text"
-          maxLength="255"
-          placeholder="********"
-          type="text"
-        />
+    <div className="flex justify-between w-full shadow-sm px-5 py-3 mb-3">
+      <div className="w-full flex items-center gap-4">
+        <h1 className="text-2xl font-semibold">Hello Osas,</h1>
+        <input
+        type="text"
+        className={` outline-none rounded-md shadow-sm focus:outline-1 focus:ring-0 px-2 py-3 my-1 block w-full md:w-3/6 placeholder:text-sm bg-[#F0F0F0]`}
+        id="text"
+        maxLength="255"
+        placeholder="Search anything...."
+        
+      />
       </div>
-      <div>dd</div>
+      <div>profile</div>
     </div>
   );
 }
