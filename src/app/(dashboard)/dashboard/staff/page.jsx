89e4bacad113da754ffcf0ea4b9f336cbe90427a
Muneerat <oladipomunirat@gmail.com>
2024-11-16 18:58:53 +0100

    feat: integrate view edit

--- src/app/(dashboard)/dashboard/staff/page.jsx
+++ src/app/(dashboard)/dashboard/staff/page.jsx
@@ -7,11 +7,60 @@ import { StaffColumns, StaffData } from "@/app/data/staffData";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Spinner from "@/app/components/Spinner";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Button } from "@/components/ui/button";
+import {
+  CaretSortIcon,
+  ChevronDownIcon,
+  DotsHorizontalIcon,
+} from "@radix-ui/react-icons"
+import handleErrors from "@/app/data/handleErrors";
 
 export default function Staff() {
   const [staffData, setStaffData] = useState([]);
   const [processing, setProcessing] = useState(false);
+  const [errors, setErrors] = useState({});
 
+  const columnsWithActions = StaffColumns.map((column) =>
+    column.id === "actions"
+      ? {
+          ...column,
+          cell: ({ row }) => (
+            <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0  ">
+                <span className="sr-only">Open menu</span>
+                <DotsHorizontalIcon className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" className="bg-white">
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Edit</DropdownMenuItem>
+              <DropdownMenuItem>Delete</DropdownMenuItem>
+              <DropdownMenuItem>View</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          ),
+        }
+      : column
+  );
+  const handleEdit = async () =>{
+    try{
+      await axios.put('api/v1/admin/user/update-profile', {user_id,first_name,last_name,email,phone,role,department})
+    }catch(error){
+      handleErrors(error,setErrors)
+    }
+
+  }
+  //fetch all users 
   useEffect(() => {
     const staffs = async () => {
       setProcessing(true);
@@ -41,6 +90,7 @@ export default function Staff() {
     };
     staffs();
   }, []);
+
   return (
     <div className="my-5">
       <BoardFilter text="Staff">
@@ -56,7 +106,7 @@ export default function Staff() {
             />
           </div>
         ) : (
-          <DataTable data={staffData} columns={StaffColumns} />
+          <DataTable data={staffData} columns={columnsWithActions} />
         )}
       </div>
     </div>
